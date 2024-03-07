0f9d2330120d0c4410cd0c5f60bee8838dc68641
Refrain <72150044+refrain-net@users.noreply.github.com>
2024-03-08 00:56:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-' forbiddenfruitをインストールすること
-' pip install forbiddenfruit
+# forbiddenfruitをインストールすること
+# pip install forbiddenfruit
 
 from forbiddenfruit import curse
 
-' matmulを実装するためのMatrixクラス定義(__getitem__とかも適宜実装する)
+# matmulを実装するためのMatrixクラス定義(__getitem__とかも適宜実装する)
 class Matrix(list):
     def __init__(self, values): self.__values = values
     def __matmul__(self, other): return Matrix(Matrix.multiply(self.__values, other.__values))
@@ -15,7 +15,7 @@ class Matrix(list):
     @staticmethod
     def multiply(mat1, mat2): return [[sum(mat1 * mat2 for mat1, mat2 in zip(mat1_row, mat2_col)) for mat2_col in zip(*mat2)] for mat1_row in mat1]
 
-' 標準オブジェクトに__matmul__を追加する
+# 標準オブジェクトに__matmul__を追加する
 curse(list, '__matmul__', lambda self, other: Matrix(self) @ Matrix(other))
 
 a = [[1, 2, 3], [4, 5, 6],]
@@ -23,4 +23,4 @@ b = [[7, 8], [9, 10], [11, 12]]
 
 c = a @ b
 print(c[0])
-' > [58, 64]
+# > [58, 64]
